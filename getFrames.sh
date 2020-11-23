b240ec649ae5f8022ce0922f251560021569180b
Kinjal Shah <kinjalshah@alumni.upenn.edu>
2020-11-23 01:42:48 -0500

    updated get frames template

--- getFrames.sh
+++ getFrames.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ffmpeg -i "/Users/kinjalshah/git/JHU/finalProject_CVDL/OptiTrip_CV/Videos/Kinjal_Fall_and_Sleep.mov" -vsync 0 "/Users/kinjalshah/git/JHU/finalProject_CVDL/OptiTrip_CV/Frames/Kinjal_Fall_and_Sleep/frame%06d.png"
+ffmpeg -i "/Users/kinjalshah/git/JHU/finalProject_CVDL/OptiTrip_CV/Videos/Kinjal_Fall_and_Sleep.mov" -vsync 0 -s 320x240 "/Users/kinjalshah/git/JHU/finalProject_CVDL/OptiTrip_CV/Frames/Kinjal_Fall_and_Sleep/frame%06d.png"
 
 
 
